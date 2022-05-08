fd7ff34cd96bda45f91e88bd59dd4fe25b534cb6
Monica Crowe <mcrowe9194@gmail.com>
2022-05-07 22:45:42 -0500

    Finished initial exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+      public Car()
+        {
+        }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public static string MakeEngineNoise(string engineNoise)
+        {
+            return engineNoise;
+        }
+        public static string MakeHonkNoise(string honkNoise)
+        {
+            return honkNoise;
+        }
+       
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -18,6 +18,40 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            var monisCar = new Car();
+            monisCar.Make = "Chevy";
+            monisCar.Model = "Equinox";
+            monisCar.Year = 2016;
+            monisCar.EngineNoise = "Vroom!!";
+            monisCar.HonkNoise = "Beep beep!!";
+            monisCar.IsDriveable = true;
+
+            var snakesCar = new Car();
+            snakesCar.Make = "Honda";
+            snakesCar.Model = "Civic";
+            snakesCar.Year = 2022;
+            snakesCar.EngineNoise = "Nyoom!";
+            snakesCar.HonkNoise = "Beep!!";
+            snakesCar.IsDriveable = true;
+
+            var audreysCar = new Car();
+            audreysCar.Make = "Little Tikes";
+            audreysCar.Model = "Cozy Coupe";
+            audreysCar.Year = 1979;
+            audreysCar.EngineNoise = "*audible childs play noise*";
+            audreysCar.HonkNoise = "HONK!!!";
+            audreysCar.IsDriveable = false;
+
+            DisplayNoise(monisCar);
+            Console.WriteLine("------------");
+            DisplayNoise(snakesCar);
+            Console.WriteLine("------------");
+            DisplayNoise(audreysCar);
+            Console.WriteLine("------------");
+
+
+
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
@@ -29,5 +63,11 @@ namespace CarLotSimulator
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
         }
+        public static void DisplayNoise(CarLotSimulator.Car car)
+        {
+            Console.WriteLine($"The {car.Make} {car.Model}'s engine sounds like {car.EngineNoise} " +
+                $"and the horn sounds like {car.HonkNoise}!");
+        }
     }
+    
 }
